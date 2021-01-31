bd52fd1dc2c588f3d36084a05d6b1ec62fb211d7
yeji <yejiyoun@sookmyung.ac.kr>
2021-01-31 18:17:19 +0900

    Add ReviewList

---
+++ app/src/main/java/com/example/direct_order/reviewlist/ReviewAdapter.java
@@ -0,0 +1,72 @@
+package com.example.direct_order.reviewlist;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.direct_order.R;
+
+import java.util.ArrayList;
+
+public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ItemViewHolder> {
+
+    // adapter에 들어갈 list 입니다.
+    private ArrayList<ReviewData> listData = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // LayoutInflater를 이용하여 전 단계에서 만들었던 item.xml을 inflate 시킵니다.
+        // return 인자는 ViewHolder 입니다.
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.review_item, parent, false);
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+        // Item을 하나, 하나 보여주는(bind 되는) 함수입니다.
+        holder.onBind(listData.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        // RecyclerView의 총 개수 입니다.
+        return listData.size();
+    }
+
+    public void addItem(ReviewData data) {
+        // 외부에서 item을 추가시킬 함수입니다.
+        listData.add(data);
+    }
+
+
+    // RecyclerView의 핵심인 ViewHolder 입니다.
+    // 여기서 subView를 setting 해줍니다.
+    class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        private RatingBar ratingBar;
+        private ImageView imageView;
+        private TextView textView;
+
+        ItemViewHolder(View itemView) {
+            super(itemView);
+
+            ratingBar = itemView.findViewById(R.id.ratingBar);
+            imageView = itemView.findViewById(R.id.imageView);
+            textView = itemView.findViewById(R.id.textView);
+
+        }
+
+        void onBind(ReviewData data) {
+            ratingBar.setRating(data.getTitle());
+            textView.setText(data.getContent());
+            imageView.setImageResource(data.getResId());
+        }
+    }
+}

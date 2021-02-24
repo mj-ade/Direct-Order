bcced13b1d5145d8dd867970b736c023787643fb
jinjuryu <fbwlswn0713@gmail.com>
2021-02-24 14:47:37 +0900

    판매자-리뷰보기 연결

--- app/src/main/java/com/example/direct_order/cake/Tab_Reviews.java
+++ app/src/main/java/com/example/direct_order/cake/Tab_Reviews.java
@@ -25,7 +25,7 @@ public class Tab_Reviews extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab__reviews_fragment, container, false);
+        return inflater.inflate(R.layout.fragment_review_list, container, false);
     }
 
     @Override

b6bc4f509d3b4c0b6621efb575ad5b1dae66b321
yeji <yejiyoun@sookmyung.ac.kr>
2021-02-26 08:30:29 +0900

    CustomerMain 데이터 반영x

---
+++ app/src/main/java/com/example/direct_order/customermain/CustomerMainAdapter.java
@@ -0,0 +1,176 @@
+package com.example.direct_order.customermain;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.direct_order.R;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerMainAdapter extends RecyclerView.Adapter<CustomerMainAdapter.ViewHolder> {
+
+    private ArrayList<CustomerMainData> mData = new ArrayList<>();
+    Context context;
+
+    public CustomerMainAdapter() {
+        this.mData = mData;
+        this.context = context;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView market_name;
+        private ImageView market_img;
+        private ImageButton likeBtn;
+        private Button orderBtn;
+        private Button reviewBtn;
+        private Button messageBtn;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            market_name = itemView.findViewById(R.id.market_name);
+            market_img = itemView.findViewById(R.id.market_img);
+            likeBtn = itemView.findViewById(R.id.likeBtn);
+            orderBtn = itemView.findViewById(R.id.orderBtn);
+            reviewBtn = itemView.findViewById(R.id.reviewBtn);
+            messageBtn = itemView.findViewById(R.id.messageBtn);
+        }
+    }
+
+
+    @NonNull
+    @Override
+    public CustomerMainAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.customermain_grid_item, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    List<String> listShopid = new ArrayList<>();
+    List<String> listMarketname = new ArrayList<>();
+    String myinsta;
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomerMainAdapter.ViewHolder holder, int position) {
+        //holder.market_img.setImageResource(mData.get(position).getImgId());
+        holder.market_name.setText(mData.get(position).getMarket_name());
+        String shopid = mData.get(position).getShopid();
+
+        FirebaseStorage storage = FirebaseStorage.getInstance("gs://direct-order-59b4e.appspot.com/");
+        StorageReference storageRef = storage.getReference();
+        storageRef.child(shopid).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                //이미지 로드 성공시
+                Glide.with(context)
+                        .load(uri)
+                        .into(holder.market_img);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                //이미지 로드 실패시
+                Toast.makeText(context, "실패", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //가게사진 누르면 인스타로 이동
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection("markets").document(shopid);
+        docRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (value != null && value.exists()) {
+                    myinsta = "https://instagram.com/" + value.get("instagram").toString().trim() + "/";
+                }
+            }
+        });
+
+        holder.market_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = view.getContext();
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(myinsta));
+                context.startActivity(intent);
+            }
+        });
+
+        //onItemClick listener here
+        holder.likeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mData.get(position).isLike()==false){
+                    holder.likeBtn.setImageResource(R.drawable.heart);//채워진하트로
+                    mData.get(position).setLike(true);
+                }else if(mData.get(position).isLike()==true){
+                    holder.likeBtn.setImageResource(R.drawable.empty);//빈하트로
+                    mData.get(position).setLike(false);
+                }
+            }
+        });
+        holder.reviewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = view.getContext();
+                //리뷰리스트 액티비티 필요--!
+                //Intent intent = new Intent(리뷰리스트액티비티);
+                //context.startActivity(intent);
+            }
+        });
+        holder.messageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //메시지기능연결
+            }
+        });
+        holder.orderBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //주문서 작성 페이지 연결
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public void addData (CustomerMainData ndata){
+        mData.add(ndata);
+    }
+
+
+}
